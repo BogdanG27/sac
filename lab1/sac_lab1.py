0768d4d760e5a69329b91bb5d329c67ea65046f2
BogdanG27 <bogdanghitan27@gmail.com>
2023-10-29 20:38:26 +0200

    added lab 3 functionalities

--- lab1/sac_lab1.py
+++ lab1/sac_lab1.py
@@ -1,13 +1,19 @@
 from recombee_api_client.api_client import RecombeeClient, Region
-from recombee_api_client.api_requests import AddItem, AddItemProperty, SetItemValues, ListItems
+from recombee_api_client.api_requests import AddItem, AddItemProperty, SetItemValues, ListItems, AddUser, AddUserProperty, SetUserValues, AddBookmark, RecommendItemsToUser
 from recombee_api_client.exceptions import APIException
 import pandas as pd
+import uuid
+import random
 
 client = RecombeeClient(
   'upb-bogdan-dev', 
   'RG145oZPeIsquQMafD1T1rZ53dyXpWBWzYPcSKk3RJQwUDLXbbJqEnKg9fPTCunO', 
   region=Region.EU_WEST
 )
+
+usernames = ['mani_docanu', 'dani_prefericitul', 'gelu_purcelu']
+users = [uuid.uuid1(), uuid.uuid1(), uuid.uuid1()]
+INTERACTIONS_NUMBER = 5
 
 df = pd.read_csv('top100cities.csv', usecols=["city", "state", "population_2020", "land_area_sqmi"])
 
@@ -53,5 +59,42 @@ def printItems():
     result = client.send(ListItems(return_properties=True))
     print(result)
 
-addItems()
-printItems()
+def addUserProperties():
+    client.send(AddUserProperty('username', 'string'))
+
+# addItems()
+# printItems()
+
+def getUser(index):
+    return users[index], usernames[index]
+
+def addUser(index):
+    userId, username = getUser(index)
+    userValues = {
+        'username': username
+    }
+    client.send(AddUser(userId))
+    client.send(SetUserValues(userId, userValues))
+    return userId
+    
+def addInteractions(userId):
+    for index, item in df.sample(n=INTERACTIONS_NUMBER).iterrows():
+        itemId = str(index)
+        try:
+            client.send(AddBookmark(str(userId), itemId))
+            print(f"User {userId} viewed item {itemId}")
+        except APIException as e:
+            print(f"Error adding view interaction: {e}")
+
+def getRecommendations(userId):
+    recommendations = client.send(RecommendItemsToUser(userId, random.randint(3, 6)))
+    recommended_items = [item['id'] for item in recommendations['recomms']]
+    return recommended_items
+
+# addUserProperties()
+
+for i in range(len(users)):
+    userId = addUser(i)
+    addInteractions(userId)
+    print(f"Recomandations for user {userId}")
+    print(getRecommendations(userId))
